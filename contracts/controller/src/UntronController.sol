d0c82e35a05ee37e373a787b4acda60e4672057e
jordan <41rjordan@proton.me>
2024-10-24 01:28:53 +0500

    basic controller contract impl

--- contracts/controller/src/UntronController.sol
+++ contracts/controller/src/UntronController.sol
@@ -10,12 +10,16 @@ contract UntronController {
     IERC20 internal immutable usdt;
 
     bytes32 internal state;
-    bytes32 internal actionChain;
+    bytes32 internal rebalanceChain;
 
-    constructor(address _verifier, bytes32 _vk, bytes32 _state) {
+    bytes internal constant RECEIVER_CONTRACT = hex"";
+
+    constructor(address _verifier, bytes32 _vk, address _usdt, bytes32 _state, bytes32 _rebalanceChain) {
         verifier = _verifier;
         vk = _vk;
+        usdt = IERC20(_usdt);
         state = _state;
+        rebalanceChain = _rebalanceChain;
     }
 
     struct Transfer {
@@ -28,33 +32,59 @@ contract UntronController {
         Transfer[] transfers;
     }
 
+    function deployReceivers(bytes32[] calldata owners) external {
+        for (uint256 i = 0; i < owners.length; i++) {
+            bytes32 salt = owners[i];
+            bytes memory bytecode = RECEIVER_CONTRACT;
+
+            assembly {
+                pop(create2(0, add(bytecode, 0x20), mload(bytecode), salt))
+            }
+        }
+    }
+
     function updateState(bytes calldata publicValues, bytes calldata proof, Rebalance[] calldata rebalances) external {
         ISP1Verifier(verifier).verifyProof(vk, publicValues, proof);
 
-        (bytes32 oldState, bytes32 newState, bytes32 oldActionChain, bytes32 newActionChain) =
-            abi.decode(publicValues, (bytes32, bytes32, bytes32, bytes32));
+        (
+            bytes32 oldState,
+            bytes32 newState,
+            bytes32 oldRebalanceChain,
+            bytes32 newRebalanceChain,
+            address oldVerifier,
+            address newVerifier,
+            bytes32 oldVk,
+            bytes32 newVk
+        ) = abi.decode(publicValues, (bytes32, bytes32, bytes32, bytes32, address, address, bytes32, bytes32));
 
         require(oldState == state);
-        require(oldActionChain == actionChain);
+        require(oldRebalanceChain == rebalanceChain);
+        require(oldVerifier == verifier);
+        require(oldVk == vk);
 
         state = newState;
+        verifier = newVerifier;
+        vk = newVk;
 
-        if (actionChain == newActionChain) {
+        if (rebalanceChain == newRebalanceChain) {
             return;
         }
-
-        actionChain = newActionChain;
 
         for (uint256 i = 0; i < rebalances.length; i++) {
             for (uint256 j = 0; j < rebalances[i].receivers.length; j++) {
                 address receiver = rebalances[i].receivers[j];
-                IERC20(usdt).transferFrom(receiver, msg.sender, IERC20(usdt).balanceOf(receiver));
+
+                usdt.transferFrom(receiver, msg.sender, usdt.balanceOf(receiver));
             }
 
             for (uint256 j = 0; j < rebalances[i].transfers.length; j++) {
                 Transfer memory transfer = rebalances[i].transfers[j];
-                IERC20(usdt).transferFrom(msg.sender, transfer.recipient, transfer.amount);
+                usdt.transferFrom(msg.sender, transfer.recipient, transfer.amount);
             }
+
+            rebalanceChain = sha256(abi.encode(rebalanceChain, rebalances[i]));
         }
+
+        require(rebalanceChain == newRebalanceChain);
     }
 }
